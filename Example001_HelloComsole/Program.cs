44fa0e9d9291e0fdcf66b95a7f39d99d79051f8d
Bormental <tyranosaurus@yandex.ru>
2022-09-15 12:11:55 +0300

    add Example001_HelloConsole project

---
+++ Example001_HelloComsole/Program.cs
@@ -0,0 +1,4 @@
+﻿// my first program c#
+Console.WriteLine("Hello, World! Bormental here");
+Console.WriteLine("dotnet new console - создать новый проект");
+Console.WriteLine ("dotnet run - запустить проект");
