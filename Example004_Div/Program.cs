5efc414baef52dac2247060b6216bc9381fb4d05
Bormental <tyranosaurus@yandex.ru>
2022-09-15 15:29:33 +0300

    Example004_Div modificated

--- Example004_Div/Program.cs
+++ Example004_Div/Program.cs
@@ -1,3 +1,7 @@
 ﻿double numberA = 12;
 double numberB = 5;
+System.Console.Write(numberA);
+System.Console.Write(" / ");
+System.Console.Write(numberB);
+System.Console.Write(" = ");
 Console.WriteLine(numberA/numberB);
