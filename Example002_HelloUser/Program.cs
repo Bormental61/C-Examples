4b1380259aa5d5329e83955b2007ac66a1763065
Bormental <tyranosaurus@yandex.ru>
2022-09-15 12:54:47 +0300

    Example002_HelloUser commands added

--- Example002_HelloUser/Program.cs
+++ Example002_HelloUser/Program.cs
@@ -2,4 +2,7 @@
 Console.WriteLine("Введите Ваше имя");
 string username = Console.ReadLine();
 Console.Write("Привет, ");
-Console.Write(username);
+Console.WriteLine(username);
+Console.WriteLine("Write(); - Вывод в одну строку");
+Console.WriteLine("WriteLine(); - Вывод и в конце перейти на новую строку");
+Console.WriteLine("ReadLine(); - Считать строку из терминала");
